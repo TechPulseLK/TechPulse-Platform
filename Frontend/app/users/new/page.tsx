aaa4232ac68b259f56628bee7884d2cfd506b892
Damitha <hellodamitha@gmail.com>
2023-11-28 20:18:48 +0530

    getting start with NextJS 14

---
+++ Frontend/app/users/new/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewUser = () => {
+  return (
+    <div>
+        <h1 className="text-4xl font-medium text-center p-10">New Users</h1>
+    </div>
+  )
+}
+
+export default NewUser
