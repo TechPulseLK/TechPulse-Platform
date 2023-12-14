e8e8c3af0b0a067538c3e54d875c13074797641a
Damitha <hellodamitha@gmail.com>
2023-12-14 13:05:08 +0530

    navbar update

--- Frontend/app/components/Navbar.tsx
+++ Frontend/app/components/Navbar.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import Link from 'next/link'
-const NavBar = () => {
+
+const Navbar = () => {
   return (
     <div>
       <nav className="bg-white  fixed w-full z-20 top-0 start-0 border-b border-gray-200">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-      
+            <img src="icon.svg" alt="Logo" className="h-8 w-8" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap ">Techpulse</span>
           </Link>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center ">Get started</button>
+            <button type="button" className="text-white bg-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-md text-sm px-4 py-2 text-center">Sign in</button>
+            
+            {/* mobile hamburger icon */}
             <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
                   <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -41,4 +44,4 @@ const NavBar = () => {
   )
 }
 
-export default NavBar
+export default Navbar
